3f3d6c19e396d4e2fed60f3e5625a0b9c85f426d
ali4xk <ma964323@gmail.com>
2026-06-03 18:45:00 +0500

    Board::checkWin func

--- main.cpp
+++ main.cpp
@@ -62,7 +62,26 @@ class Board
     {
 
     }
-    bool checkWin(char symbol);
+    bool checkWin(char symbol)
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            if(grid[i][0] == symbol && grid[i][1] == symbol && grid[i][2] == symbol)
+            return true;
+        }
+        for(int j = 0; j < 3; j++)
+        {
+            if(grid[0][j] == symbol && grid[1][j] == symbol && grid[2][j] == symbol)
+            return true;
+        }
+        if(grid[0][0] == symbol && grid[1][1] == symbol && grid[2][2] == symbol)
+        return true;
+
+        if(grid[0][2] == symbol && grid[1][1] == symbol && grid[2][0] == symbol)
+        return true;
+
+        return false;
+    }
     void reset();
 };
 class Game

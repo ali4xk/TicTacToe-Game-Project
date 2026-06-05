aeedab6f50f8660178d525d06878514be63a9d9e
ali4xk <ma964323@gmail.com>
2026-06-05 20:13:54 +0500

    Improving Interface(1)

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,28 +1,41 @@
-#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
 const int WINDOW_SIZE = 600;
 const int GRID_SIZE = 3;
 const float CELL_SIZE = WINDOW_SIZE / GRID_SIZE;
+
 class Game
 {
 private:
     sf::RenderWindow window;
+    sf::Font font;
     char board[3][3];
+    bool isXTurn = true;
+
 public:
     Game();
     void run();
-    void render();
     void handleEvents();
+    void render();
 };
 
-Game::Game() : window(sf::VideoMode({600, 600}), "Tic Tac Toe")
+Game::Game()
+    : window(sf::VideoMode({WINDOW_SIZE, WINDOW_SIZE}), "Tic Tac Toe")
 {
+    // init board
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
             board[i][j] = ' ';
         }
+    }
+
+    // load font
+    if (!font.openFromFile("arial.ttf"))
+    {
+        std::cout << "Font failed to load!" << std::endl;
     }
 }
 
@@ -37,29 +50,7 @@ void Game::run()
         window.display();
     }
 }
-void Game::render()
-{
-    sf::RectangleShape line;
-    line.setFillColor(sf::Color::Black);
 
-    float thickness = 6.f;
-
-    // vertical lines
-    for (int i = 1; i < GRID_SIZE; i++)
-    {
-        line.setSize(sf::Vector2f(thickness, WINDOW_SIZE));
-        line.setPosition(sf::Vector2f(i * CELL_SIZE, 0.f));
-        window.draw(line);
-    }
-
-    // horizontal lines
-    for (int i = 1; i < GRID_SIZE; i++)
-    {
-        line.setSize(sf::Vector2f(WINDOW_SIZE, thickness));
-        line.setPosition(sf::Vector2f(0.f, i * CELL_SIZE));
-        window.draw(line);
-    }
-}
 void Game::handleEvents()
 {
     while (auto event = window.pollEvent())
@@ -74,14 +65,66 @@ void Game::handleEvents()
             int col = mousePos.x / CELL_SIZE;
             int row = mousePos.y / CELL_SIZE;
 
-            std::cout << "Clicked cell: " << row << ", " << col <<std:: endl;
+            if (row >= 0 && row < 3 && col >= 0 && col < 3)
+            {
+                if (board[row][col] == ' ')
+                {
+                    board[row][col] = (isXTurn ? 'X' : 'O');
+                    isXTurn = !isXTurn;
+                }
+            }
         }
     }
 }
+
+void Game::render()
+{
+    // GRID
+    sf::RectangleShape line;
+    line.setFillColor(sf::Color::Black);
+
+    float thickness = 6.f;
+
+    for (int i = 1; i < GRID_SIZE; i++)
+    {
+        line.setSize(sf::Vector2f(thickness, WINDOW_SIZE));
+        line.setPosition(sf::Vector2f(i * CELL_SIZE, 0.f));
+        window.draw(line);
+    }
+
+    for (int i = 1; i < GRID_SIZE; i++)
+    {
+        line.setSize(sf::Vector2f(WINDOW_SIZE, thickness));
+        line.setPosition(sf::Vector2f(0.f, i * CELL_SIZE));
+        window.draw(line);
+    }
+
+    // TEXT (X and O)
+    sf::Text text(font, "", 80);
+    text.setFillColor(sf::Color::Black);
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (board[i][j] != ' ')
+            {
+                text.setString(board[i][j]);
+
+                text.setPosition(sf::Vector2f(
+                    j * CELL_SIZE + 70.f,
+                    i * CELL_SIZE + 40.f
+                ));
+
+                window.draw(text);
+            }
+        }
+    }
+}
+
 int main()
 {
     Game game;
     game.run();
-
     return 0;
 }
